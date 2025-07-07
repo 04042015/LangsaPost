75a14143c76bff316574a136e320fd4765c15b38
04042015 <m.iggopramulia14@gmail.com>
2025-07-08 02:07:54 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+  <NavBar />
+  {children}
+</body>
     </html>
   )
 }
