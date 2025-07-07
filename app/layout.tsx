18fe9e3ca435310441405205ff793dcb78d8c9a7
04042015 <m.iggopramulia14@gmail.com>
2025-07-07 19:33:45 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,11 +49,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="id">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-      <body className="antialiased bg-white text-black">{children}</body>
+    <html lang="id" className="scroll-smooth">
+      <body className="antialiased">{children}</body>
     </html>
   )
-                                   }
+}
