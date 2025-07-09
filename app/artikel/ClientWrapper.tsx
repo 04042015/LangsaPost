3c549239b3190d8dc38653128d13018c808649e9
04042015 <m.iggopramulia14@gmail.com>
2025-07-10 04:03:18 +0700

    Update ClientWrapper.tsx

--- app/artikel/ClientWrapper.tsx
+++ app/artikel/ClientWrapper.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import dynamic from "next/dynamic"
-import type { Article } from "./types" // kamu bisa pindahkan interface ke file types.ts agar bersih
+import type { Article } from "./types"
 
+// Import komponen client-side
 const ArtikelClient = dynamic(() => import("./ArtikelClient"), { ssr: false })
 
 export default function ClientWrapper({ articles }: { articles: Article[] }) {
