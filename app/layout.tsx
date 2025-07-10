4001839ce46e0d90c7ef60a8c4c2ef1068b1d154
04042015 <m.iggopramulia14@gmail.com>
2025-07-11 03:04:45 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Header from './_components/Header'
+import Header from "@/components/Header"
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
