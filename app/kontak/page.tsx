1237903fe4e84a7a84ae0767b548f60da5abee65
04042015 <m.iggopramulia14@gmail.com>
2025-07-10 23:00:08 +0700

    Update page.tsx

--- app/kontak/page.tsx
+++ app/kontak/page.tsx
@@ -1,6 +1,9 @@
-'use client';
+import dynamic from 'next/dynamic';
 
-import KontakClient from './KontakClient';
+const KontakClient = dynamic(() => import('./KontakClient'), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 
 export default function Page() {
   return <KontakClient />;
