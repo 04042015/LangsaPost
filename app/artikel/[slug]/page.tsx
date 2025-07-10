0b89e9c0a0b01441e315adb83624089d26ec8a96
04042015 <m.iggopramulia14@gmail.com>
2025-07-11 02:30:28 +0700

    Update page.tsx

--- app/artikel/[slug]/page.tsx
+++ app/artikel/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import Header from '@/_components/Header'
+import Header from "@/components/Header"
 import CategoryNav from "@/components/CategoryNav"
 import Footer from "@/components/Footer"
 import ArticleCard from "@/components/ArticleCard"
