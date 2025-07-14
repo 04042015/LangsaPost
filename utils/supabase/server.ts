1a6bfe2af09af3c277d21dc0c37ddeebf7e4f2ba
04042015 <m.iggopramulia14@gmail.com>
2025-07-15 01:44:17 +0700

    Update server.ts

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,6 +1,13 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { cookies } from "next/headers"
+import { cookies, headers } from "next/headers"
+import { createServerClient } from "@supabase/ssr"
 
-export function createClient() {
-  return createServerComponentClient({ cookies })
+export const createClient = () => {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies,
+      headers,
+    }
+  )
 }
