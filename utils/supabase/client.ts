8b1289d2fa4a9d012e98e142b69527c5b509f727
04042015 <m.iggopramulia14@gmail.com>
2025-07-15 01:43:30 +0700

    Update client.ts

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,5 +1,9 @@
-import { createBrowserClient } from "@supabase/auth-helpers-nextjs"
+import { createBrowserClient } from '@supabase/ssr'
+import { createClientComponentClient } from '@supabase/ssr'
 
-export function createClient() {
-  return createBrowserClient()
+export const createClient = () => {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
 }
