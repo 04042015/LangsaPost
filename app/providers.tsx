9625b26389368cfee4669e744a2594a68ed14019
04042015 <m.iggopramulia14@gmail.com>
2025-07-15 02:28:14 +0700

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,16 @@
 "use client"
 
-import { SessionContextProvider } from "@supabase/auth-helpers-react"
+import { createBrowserClient } from "@supabase/ssr"
+import { SessionContextProvider } from "@supabase/ssr"
 import { useState } from "react"
-import { createClient } from "@/utils/supabase/client"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [supabase] = useState(() => createClient())
+  const [supabase] = useState(() =>
+    createBrowserClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+  )
 
   return (
     <SessionContextProvider supabaseClient={supabase}>
