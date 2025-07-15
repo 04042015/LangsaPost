f13ad1dcd9f332cc0ca44555dcd744fa95943be7
04042015 <m.iggopramulia14@gmail.com>
2025-07-15 18:07:15 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -102,7 +102,7 @@ export default function HomePage() {
     <div className="min-h-screen bg-gray-50">
       <Header />
       <CategoryNav />
-      {/* <LiveBreakingNews /> */}
+      <LiveBreakingNews />
 
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {featuredArticle && (
@@ -198,7 +198,7 @@ export default function HomePage() {
       </main>
 
       {/* <PWAInstallPrompt /> */}
-      {/* <LiveChat /> */}
+       <LiveChat />
       <Footer />
     </div>
   )
